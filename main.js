f411081eeefc5dbbddb8a2feaeb34f08f14fe9be
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-09-25 12:03:44 -0300

    w

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ function areaQuadrado (lado){
 }
 console.log (areaQuadrado(4))
 
-let media= function (num6,num2){
-    return num6+num2/2;
+let media= function (num6,num2,num8){
+    return num6+num2+num8/2;
 }
-console.log (media (6,2));
+console.log (media (6,2,8));
+
 
 
