1dd4504b85392f1a761d4e55f4b72dcf044dac49
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-02 11:32:27 -0300

    .

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-function soma (num1,num2){
-    return num1+num2;
-}
-console.log (soma(1,2));
+// function soma (num1,num2){
+//     return num1+num2;
+// }
+// console.log (soma(1,2));
 
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log (areaQuadrado(4))
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log (areaQuadrado(4))
 
-let media= function (num6,num2,num8){
-    return num6+num2+num8/3;
-}
-console.log (media (6,2,8));
+// let media= function (num6,num2,num8){
+//     return num6+num2+num8/3;
+// }
+// console.log (media (6,2,8));
 
-let nome=prompt('toretto')
-document.write(toretto)
-parseInt(prompt('qual a media?'));
-console.log (nome (toretto));
+// let nome=prompt('toretto')
+// document.write(toretto)
+// parseInt(prompt('qual a media?'));
+// console.log (nome (toretto));
+
+let estados={'AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'}
