5c9ec4b8ac702d5c52d2e2997575a2dee2e4e1ae
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-09 11:47:28 -0300

    a

---
+++ pergunta.js
@@ -0,0 +1,80 @@
+criaCartao(
+    'Programaçao',
+    'O que é uma variável em programação?',
+    'É um espaço de armazenamento')
+    criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'É uma estrutura de controle que repete a execução de um bloco de código')
+    criaCartao(
+    'Programação',
+    'Qual sua linguagem favorita?',
+    'Java')
+    criaCartao(
+    'Programação',
+    'O que um algoritmo?',
+    'Sequencia finita de instruções')
+    criaCartão(
+    'Programação',
+    'O que é bug?',
+    'Falha no código de programação')
+    criaCartao(
+    'Programação',
+    'O que é um jit?',
+    'Sistema de controle para acompanhar mudança no código')
+    criaCartao(
+    'Programação',
+    'O que é um banco de dados?',
+    'Uma coleção organizada de dados')
+    criaCartao(
+    'Programação',
+    'O que é um comentário no código?',
+    'Anotação no código que é ignorada pelo computador')
+    criaCartao(
+    'Programaçao',
+    'O que é uma variável?',
+    'Espaço de armazenamento que contem dados')
+    criaCartao(
+    'Programação',
+    'O que significa loop em programação?',
+    'Estrutura que permite repetir bloco')
+    criaCartao(
+    'Programação',
+    'O que é Html?',
+    'Linguagem padrão para criar paginas web')
+    criaCartao(
+    'Programação',
+    'O que é um framework?',
+    ' Suporte para desenvolver aplicativos')
+    criaCartao(
+    'Programação',
+    'O que é um servidor?',
+    'Sistema que oferece recursos a outros computadores')
+    criaCartao(
+    'Programação',
+    'O que são variáveis globais?',
+    ' Variáveis definidas fora de qualquer função')
+    criaCartao(
+    'Programação',
+    'O que é uma classe?',
+    'Modelo de propriedade de métodos')
+    criaCartao(
+    'Programação',
+    'O que são testes unitários?',
+    'Testes automáticos que verificam se programas funcionam corretamente')
+    criaCartao(
+    'Programaçao',
+    'Qual sua linguagem favorita?',
+    'Python')
+    criaCartao(
+    'Programação',
+    'Qual sua linguagem favorita?',
+    ' Javascript')
+    criaCartao(
+    'Programação',
+    'Qual sua linguagem favorita?',
+    'C++')
+    criaCartao(
+    'Programação',
+    'Qual sua linguagem favorita?',
+    ' Java')
