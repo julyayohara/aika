f887402bf4a4ebe76eab76e6594718dfe575d4e0
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-09 12:09:03 -0300

    a

--- main.js
+++ main.js
@@ -1,40 +1,14 @@
-// function soma (num1,num2){
-//     return num1+num2;
-// }
-// console.log (soma(1,2));
-
-// function areaQuadrado (lado){
-//     return lado**2;
-// }
-// console.log (areaQuadrado(4))
-
-// let media= function (num6,num2,num8){
-//     return num6+num2+num8/3;
-// }
-// console.log (media (6,2,8));
-
-// let nome=prompt('toretto')
-// document.write(toretto)
-// parseInt(prompt('qual a media?'));
-// console.log (nome (toretto));
-
-// let estados=['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
-// let i=0;
-// while(i<estados.length){
-//     console.log(i);
-//     console.log(estados [i]);
-//     i++;
-// }
-// for (let i= 0; estados.length; i++) {
-   
-//     console.log(i);
-//     console.log(estados[i]);
-// }
-let idade=16;
-if(idade>17){
-    console.log('pode dirigir');
-
-}
-else{
-    console.log('ilegal dirigir');
-}
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartão = document.createElement('article');
+    cartão.className = 'cartão'
+    cartão.innerHTML =
+    <div class="conteudo-cartão">
+    <h3>categoria</h3>
+<div class="pergunta-cartão">
+<p>pergunta</p>
+</div>
+    <div class="resposta-cartõ">
+        <p>resposta</p>
+    </div>
+</div>
