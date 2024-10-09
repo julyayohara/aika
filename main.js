37112c0d8de694a2bbdc4f92def63b61c6e77bf5
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-09 12:12:21 -0300

    x

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta)
     let container = document.getElementById('container');
     let cartão = document.createElement('article');
     cartão.className = 'cartão'
