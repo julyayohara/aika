ed7844fc188b70919d5f7dd4e755c0158bfc39db
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-16 11:32:12 -0300

    a

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function criacartao(categoria, pergunta, resposta)
+function criacartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-cartao.innerHTML = `
+cartao.innerHTML = 
 <div class="conteudo-cartao">
     <h3>categoria</h3>
 <div class ="pergunta-cartao">
