931b33503c3500125dd2c456b28c88dbace32b2b
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-29 17:46:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-function criarcartao(categoria, pergunta, resposta) {
+function criarcartao(categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.respostaEstaVisivel = false;  // Define a propriedade no cartão
-
     cartao.innerHTML = `
         <div class="conteudo-cartao">
             <h3>${categoria}</h3>
             <div class="pergunta-cartao">
                 <p>${pergunta}</p>
-            </div>
         </div>
         <div class="resposta-cartao">
             <p>${resposta}</p>
         </div>
-    `;
-
-    function viracartao() {
+    `
+let respostaEstaVisivel = false;
+    function viracartao(){
         cartao.respostaEstaVisivel = !cartao.respostaEstaVisivel;
-        cartao.classList.toggle('active', cartao.respostaEstaVisivel);
+        cartao.classList.toggle('active',cartao.respostaEstaVisivel);
     }
 
     cartao.addEventListener('click', viracartao);
