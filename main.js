e174bc1098e576d7dc66f9af0075feea7109e6f0
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-29 08:16:11 -0300

    a

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-function criacartao(categoria,pergunta,resposta){
+function criarcartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-cartao.innerHTML = `
-<div class="conteudo-cartao">
-    <h3>${categoria}</h3>
-<div class ="pergunta-cartao">
-<p>${pergunta}</p>
-</div>
-    <div class = "resposta-cartao">
-        <p>${resposta}</p>
-    </div>
-</div>
-`
-   let respostaEstaVisivel = false;
-   function viracartao(){
-    respostaEstaVisivel = !respostaEstaVisivel;
-    cartao.classList.toggle('active',respostaEstaVisivel);
-   }
-   cartao.addEventListener('click', viracartao);
+    cartao.innerHTML = `
+        <div class="conteudo-cartao">
+            <h3>${categoria}</h3>
+            <div class="pergunta-cartao">
+                <p>${pergunta}</p>
+            </div>
+            <div class="resposta-cartao">
+                <p>${resposta}</p>
+            </div>
+        </div>
+    `;
 
-container.appendChild(cartao);
+    let respostaEstaVisivel = false;
+    function viracartao(){
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
+    cartao.addEventListener('click', viracartao);
+    container.appendChild(cartao);
 }
