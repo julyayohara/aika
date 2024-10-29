2bb19add8a7e9d919b04f01be3334dc2866a2b90
julyayohara <142261279+julyayohara@users.noreply.github.com>
2024-10-29 17:56:42 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,80 +1,100 @@
     criacartao(
     'Programaçao',
     'O que é uma variável em programação?',
-    'É um espaço de armazenamento')
+    'É um espaço de armazenamento'
+    )
     criacartao(
     'Programação',
     'O que é um loop?',
-    'É uma estrutura de controle que repete a execução de um bloco de código')
+    'É uma estrutura de controle que repete a execução de um bloco de código'
+    )
     criacartao(
     'Programação',
     'Qual sua linguagem favorita?',
-    'Java')
+    'Java'
+    )
     criacartao(
     'Programação',
     'O que um algoritmo?',
-    'Sequencia finita de instruções')
+    'Sequencia finita de instruções'
+    )
     criacartao(
     'Programação',
     'O que é bug?',
-    'Falha no código de programação')
+    'Falha no código de programação'
+    )
     criacartao(
     'Programação',
     'O que é um jit?',
-    'Sistema de controle para acompanhar mudança no código')
+    'Sistema de controle para acompanhar mudança no código'
+    )
     criacartao(
     'Programação',
     'O que é um banco de dados?',
-    'Uma coleção organizada de dados')
+    'Uma coleção organizada de dados'
+    )
     criacartao(
     'Programação',
     'O que é um comentário no código?',
-    'Anotação no código que é ignorada pelo computador')
+    'Anotação no código que é ignorada pelo computador'
+    )
     criacartao(
     'Programaçao',
     'O que é uma variável?',
-    'Espaço de armazenamento que contem dados')
+    'Espaço de armazenamento que contem dados'
+    )
     criacartao(
     'Programação',
     'O que significa loop em programação?',
-    'Estrutura que permite repetir bloco')
+    'Estrutura que permite repetir bloco'
+    )
     criacartao(
     'Programação',
     'O que é Html?',
-    'Linguagem padrão para criar paginas web')
+    'Linguagem padrão para criar paginas web'
+    )
     criacartao(
     'Programação',
     'O que é um framework?',
-    ' Suporte para desenvolver aplicativos')
+    ' Suporte para desenvolver aplicativos'
+    )
     criacartao(
     'Programação',
     'O que é um servidor?',
-    'Sistema que oferece recursos a outros computadores')
+    'Sistema que oferece recursos a outros computadores'
+    )
     criacartao(
     'Programação',
     'O que são variáveis globais?',
-    ' Variáveis definidas fora de qualquer função')
+    ' Variáveis definidas fora de qualquer função'
+    )
     criacartao(
     'Programação',
     'O que é uma classe?',
-    'Modelo de propriedade de métodos')
+    'Modelo de propriedade de métodos'
+    )
     criacartao(
     'Programação',
     'O que são testes unitários?',
-    'Testes automáticos que verificam se programas funcionam corretamente')
+    'Testes automáticos que verificam se programas funcionam corretamente'
+    )
     criacartao(
     'Programaçao',
     'Qual sua linguagem favorita?',
-    'Python')
+    'Python'
+    )
     criacartao(
     'Programação',
     'Qual sua linguagem favorita?',
-    ' Javascript')
+    ' Javascript'
+    )
     criacartao(
     'Programação',
     'Qual sua linguagem favorita?',
-    'C++')
+    'C++'
+    )
     criacartao(
     'Programação',
     'Qual sua linguagem favorita?',
-    ' Java')
+    ' Java'
+    )
