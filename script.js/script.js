98e4808350a9ba491ec76b3a0a4412c555c2c1d3
julyayohara <142261279+julyayohara@users.noreply.github.com>
2025-03-11 17:57:48 -0300

    as

--- script.js/script.js
+++ script.js/script.js
@@ -6,3 +6,13 @@ function toggleMenu() {
         menu.style.display = "block";
     }
 }
+
+function mostrarConteudo(materia) {
+    // Oculta todos os conteúdos primeiro
+    document.querySelectorAll('.conteudo').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Exibe apenas o conteúdo da matéria selecionada
+    document.getElementById(materia).style.display = 'block';
+}
